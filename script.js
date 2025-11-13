8704998e641696da420170375cf761d9e4e926bb
Mason Quinn <mason117167@student-schoolsofwestfield.org>
2025-11-13 14:41:44 +0000

    stuff added

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+/* main controls the program. 
+ * Store the number of questions to ask in a variable called questions.
+ * Call askQuestions, sending questions as an argument, which returns the number right. 
+ * Give feedback depending on the number right returned: Either "Perfect!" or say 
+ * how many right out of the number asked (questions). 
+ * @param none
+ * @return none
+ */
