305289b75f18e148cb11f76ad960e298a7a787fb
Mason Quinn <mason117167@student-schoolsofwestfield.org>
2025-11-13 14:56:21 +0000

    documentation begun

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 /* main controls the program. 
- * Store the number of questions to ask in a variable called questions.
- * Call askQuestions, sending questions as an argument, which returns the number right. 
- * Give feedback depending on the number right returned: Either "Perfect!" or say 
- * how many right out of the number asked (questions). 
+ * create two global arrays, one for substitutes, and one for the story,
+ * then create an empty global for storyWithSpaces. Run the program. 
  * @param none
  * @return none
+ */
+
+
+ // Call encodeStory
+ // Call spaceMarks
+ // let numberedList = subSpeech()
+ // call showSpeechList with numberedList as a parameter
+ // alert the user that setup is complete and that the player can now start playing
+ // call rebuildStory, then create a new variable that connects the story array together with spaces, defining storyWithSpaces as join
+ // showStory(), then thank the player and end.
+
+ /* getStory()
+  * user input is stored as a new variable, that variable is returned.
+  * @param none
+  * @return storyString
+  */
+
+ /* encodeStory(storyString)
+ * create a copy of storyString as a new variable, story array is equal to storyString being split by spaces
+ * story = spaceMarks(story)
+ * return string
+ * @param storyString
+ * @return story, string
  */
